93fcc268a57e59af408b67af5efd3fd36187c16b
Bob Zheng <zhengbangbo@hotmail.com>
2024-03-11 13:06:59 +0800

    Update (#4)

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,9 +1,6 @@
 'use client'
 
 import { useTranslations } from 'next-intl'
-import Image from 'next/image'
-import Fuwu from './aidbo-fuwu.png'
-import ZBJ from './aidbo-zbj.png'
 
 export default function Home() {
   const t = useTranslations('Home')
@@ -12,21 +9,6 @@ export default function Home() {
     <>
       <h1>👋&nbsp;{t('Heading')}</h1>
       <p>{t.rich('Paragraph1')}</p>
-      <a href="https://www.zbj.com/dp/32042712/" target="_blank">
-        <Image src={ZBJ} alt="Aidbo Fuwu" placeholder="blur" />
-      </a>
-      <p className="-mt-4 text-center text-sm text-stone-400 dark:text-stone-500">
-        {t('CaptionZBJ')}
-      </p>
-      <a
-        href="https://fuwu.weixin.qq.com/profile/sq_oWTzf6t7Vrzf099ONaqXS5vMc0L4"
-        target="_blank"
-      >
-        <Image src={Fuwu} alt="Aidbo Fuwu" placeholder="blur" />
-      </a>
-      <p className="-mt-4 text-center text-sm text-stone-400 dark:text-stone-500">
-        {t('Caption')}
-      </p>
     </>
   )
 }
